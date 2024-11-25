c6c24a8852197e9841eec695b207c85a131e1b52
Brendon <brendonpjon@gmail.com>
2024-11-25 16:43:42 +0000

    images are being sent to db

---
+++ src/index.js
@@ -0,0 +1,67 @@
+import express from "express"
+import { dirname, join } from 'path';
+import {fileURLToPath} from 'url';
+import multer from "multer";
+import 'dotenv/config'
+
+import cloudinary from "./util/cloudinary.js";
+
+
+
+
+const storage = multer.diskStorage({
+    destination: function (req,file,cb)
+    {
+        console.log(file)
+        cb(null, 'uploads/');
+    },
+    filename: function (req,file,cb){
+        console.log(file)
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({ storage })
+
+
+const app = express()
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/test", upload.array('data', 20), (req, res) => {
+    req.files.forEach(element => {
+        cloudinary.uploader.upload(element.path, function (err,result){
+            if(err)
+            {
+                console.log(err)
+                return res.status(500).json({
+                    success: false,
+                    message: "Error"
+                })
+            }
+    
+            res.status(200).json({
+                data:result
+            })
+    })
+    
+    
+    
+    });
+})
+
+
+app.get("/showfile", (req,res) => { 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    res.sendFile(join(__dirname, 'index.html'));
+});
+
+
+
+
+app.listen("3000" ,() =>{
+    console.log("running on 3000")
+})
